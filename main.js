22de8fb31bcb50b575af047405691621bc6a9b8f
Joe Binder <jbinder@jbindermac.local>
2016-07-15 08:40:30 -0700

    Clean up formatting

--- main.js
+++ main.js
@@ -2,20 +2,11 @@
 /*jshint unused:true */
 
 /*
-A simple node.js application intended to blink the onboard LED on the Intel based development boards such as the Intel(R) Galileo and 
-Edison using the onboard LED
-
-MRAA - Low Level Skeleton Library for Communication on GNU/Linux platforms
-Library in C/C++ to interface with Galileo & other Intel platforms, in a structured and sane API with port nanmes/numbering that match boards & with bindings to javascript & python.
-
-Steps for installing MRAA & UPM Library on Intel IoT Platform with IoTDevKit Linux* image
-Using a ssh client: 
-1. echo "src maa-upm http://iotdk.intel.com/repos/1.1/intelgalactic" > /etc/opkg/intel-iotdk.conf
-2. opkg update
-3. opkg upgrade
-
-Article: https://software.intel.com/en-us/html5/articles/intel-xdk-iot-edition-nodejs-templates
-*/
+ * A simple node.js application intended to blink the onboard LED on the Intel based i
+ * development boards such as the Intel(R) Galileo and Edison using the onboard LED.
+ *  
+ * https://software.intel.com/en-us/html5/articles/intel-xdk-iot-edition-nodejs-templates
+ */
 
 var mraa = require('mraa'); //require mraa
 
