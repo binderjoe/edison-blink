528fb87854fac4e2a7fe5f14bbd67f761e830894
binderjoe <binderjoe@users.noreply.github.com>
2016-07-14 21:22:21 -0700

    Update config.js
    Update config to use environment variables

--- config.js
+++ config.js
@@ -1,10 +1,14 @@
 var fs = require('fs');
+var os = require('os');
+var path = require('path');
+
+var sshKeyPath = path.join(os.homedir(), '.ssh/', 'id_edison');
 
 module.exports = {
-    host: 'hostname',
+    host: '192.168.0.18',
     user: 'root',
     projectName: '~/node-blink/',
-    privateKey: fs.readFileSync('sshkey'),
+    privateKey: fs.readFileSync(sshKeyPath),
     startFile: 'main.js',
     sshPort: 22
 }
